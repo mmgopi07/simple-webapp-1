9c3186f0f3f851a714dd0e45f1a2a78182d8a77c
mmgopi07 <mm.gopi92@gmail.com>
2023-03-19 13:04:48 +0530

    Enhance

--- Simple-webapp/src/main/java/com/webapp/controller/MainController.java
+++ Simple-webapp/src/main/java/com/webapp/controller/MainController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 	
-	@RequestMapping("/wish")
+	@RequestMapping("/sam")
 	public String wish() {
 		
-		return "wish";
+		return "sam";
 	}
 
 }
